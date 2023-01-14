fd221c379783472aa54436e7709466a67861e167
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2023-01-14 18:22:11 +0100

    init

---
+++ components/App/AppFooter.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer>footer</footer>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Nuxt Blueprint",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "This is my first Nuxt App",
+        },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+  css: ["@/assets/style/app.scss"],
+  modules: ["@nuxtjs/tailwindcss"],
+})
